c006549902bff8266a73f7f9dbbe5fd29b5f9ac1
AlexandrKlepinin <alexandrklepinin@gmail.com>
2025-07-28 06:16:40 +0500

    Prealpha

--- tests/validators/test_validators.py
+++ tests/validators/test_validators.py
@@ -4,17 +4,18 @@ from tests.validators.conftest import NAMES_TEST_LOG_FILES
 from validators.validators import date_validation, files_exists, report_validation
 
 
-@pytest.mark.parametrize("log_files, result", [(NAMES_TEST_LOG_FILES, None)])
+@pytest.mark.parametrize("log_files, result", [(NAMES_TEST_LOG_FILES, True)])
 def test_files_validator_good(create_log_file, log_files, result) -> None:
     assert files_exists(log_files) is result
 
 
 @pytest.mark.parametrize(
-    "log_files, result",
-    [("test_log_file.log", LogFileNotExistError("test_log_file.log"))],
+    "log_file, result",
+    [("test_log_file.log", LogFileNotExistError)],
 )
 def test_files_validator_bad(log_file, result) -> None:
-    assert files_exists(log_file) == result
+    with pytest.raises(result):
+        files_exists(log_file) == result
 
 
 @pytest.mark.parametrize(
@@ -22,8 +23,7 @@ def test_files_validator_bad(log_file, result) -> None:
     [
         ("average", None),
         ("Average", None),
-        ("  AVERAGE  ", None),
-        ("AVARAGA", ReportError("AVARAGA")),
+        ("  AVERAGE  ", None)
     ],
 )
 def test_report_validation_good(report, result) -> None:
@@ -31,7 +31,26 @@ def test_report_validation_good(report, result) -> None:
 
 
 @pytest.mark.parametrize(
-    "date, result", [("2025-22-06", None), ("06-22-2025", DateError("06-22-2025"))]
+    "report, result",
+    [
+        ("AVARAGA", ReportError),
+    ],
+)
+def test_report_validation_bad(report, result) -> None:
+    with pytest.raises(result):
+        report_validation(report)
+
+
+@pytest.mark.parametrize(
+    "date, result", [("2025-22-06", None),]
 )
 def test_date_validation_good(date, result) -> None:
     assert date_validation(date) is result
+
+
+@pytest.mark.parametrize(
+    "date, result", [("06-22-2025", DateError)]
+)
+def test_date_validation_bad(date, result) -> None:
+    with pytest.raises(result):
+        date_validation(date)
